a4837373c9d2080e8bddb9133f60b2b23994f2cb
Udev2002 <udayanga.dev.lk@gmail.com>
2024-05-03 23:57:09 -0700

    change to responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,283 @@
-import Image from 'next/image'
+import Image from "next/image";
+import head from "next/head";
+import React from "react";
+
+//import images
+import profileimage from "/public/p2.png";
+import designimage from "/public/design.png";
+import code from "/public/code.png";
+import web1 from "/public/web1.png";
+import web2 from "/public/web2.png";
+import web3 from "/public/web3.png";
+
+//import framer motion
+import { motion } from "framer-motion";
+// import icon
+import { RiMoonClearFill } from "react-icons/ri";
+import { BsGithub, BsLinkedin, BsInstagram, BsTwitter } from "react-icons/bs";
+
+//import hook
 
 export default function Home() {
+  //function
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <div>
+      <head>
+        <title>create next app</title>
+
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;500&display=swap');
+        </style>
+      </head>
+      <main className="font-poppins bg-yellow-50 min-h-screen px-10 md:px-20 lg:px-30 ">
+        {/*mulinma hadanne moblile eketa adala light mode ek */}
+
+        {/*navigation and hero  */}
+        <section>
+          {/* nav */}
+          <nav className="flex justify-between py-10 ">
+            <h1 className="  text-1xl font-bold md:text-2xl">
+              Develope next app by UDT
+            </h1>
+            <ul className=" flex items-center">
+              {/* icon import react icon*/}
+              <li>
+                <RiMoonClearFill
+                  //onclick function
+
+                  className="
+                 cursor-pointer text-xl mx-5 "
+                />
+              </li>
+              <li className="">
+                <a
+                  href=""
+                  className="bg bg-gradient-to-b from-pink-600 to-slate-900 px-5 py-2 text-white rounded-3xl"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          {/* hero section */}
+          <div className=" text-center py-5 ">
+            <h1 className=" text-5xl text-bold font-bold text-pink-800 md:text-7xl lg:mb-4">
+              Udayanga Developer Team
+            </h1>
+
+            <h3 className="text-2xl font-poppins text-black py-4 md:mx-auto md:text-3xl md:mt-4">
+              Mobile App and website Developer
+            </h3>
+            <p className="text-sm text-gray-500 leading-5 py-2 md:text-lg lg:text-xl mx-auto max-w-xl">
+              {" "}
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. In
+              voluptatibus, nam rerum, sequi minima voluptates, rem blanditiis
+              magnam eaque dolores laborum quisquam eos. Impedit nobis labore
+              natus dolore mollitia. Facere? sample paragraph sample paragraph
+              sample paragraph sample paragraph sample paragraph sample
+              paragraph sample paragraph sample paragraph sample paragraph
+              sample paragraph sample paragraph sample paragraph
+            </p>
+          </div>
+          {/* social media icons */}
+          <div className="flex justify-center gap-10 text-3xl px-20 py-5 text-blue-950 ">
+            <a href="">
+              <BsGithub />
+            </a>
+            <a href="">
+              <BsLinkedin />
+            </a>
+            <a href="">
+              <BsTwitter />
+            </a>
+            <a href="">
+              <BsInstagram />
+            </a>
+          </div>
+          {/* profile image */}
+
+          <div className="w-80 h-80 m-auto relative pt-5 mt-20 bg-gradient-to-b from-pink-600 to-slate-900 rounded-full">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              objectFit="cover"
+              layout="fill"
+              src={profileimage}
+              alt="profile image"
             />
-          </a>
+          </div>
+        </section>
+        {/* service section */}
+        <section>
+          <div className="py-10">
+            <h2 className=" font-poppins font-bold text-gray-600 text-2xl md:text-3xl ">
+              Services I offer
+            </h2>
+            <p className="text-sm text-gray-800 leading-5 py-2 lg:text-lg">
+              sample paragraph sample paragraph sample paragraph sample
+              paragraph sample paragraph sample paragraph sample paragraph
+              sample paragraph sample paragraph sample paragraph sample
+              paragraph sample paragraph
+            </p>
+          </div>
+          {/* services cards */}
+          <div className="md:flex lg:flex mx-auto gap-10">
+            <div>
+              {/* card one */}
+              <div className="bg-gradient-to-b from-pink-800 to-slate-900 text-center rounded-2xl my-5 shadow-sm p-5">
+                <Image
+                  src={designimage}
+                  alt="designimage"
+                  width={100}
+                  height={100}
+                  className="inline"
+                />
+                <h2 className="text-2xl font-bold text-black">
+                  Graphics Desingning
+                </h2>
+                <p className="text-sm py-2 text-white opacity-20 leading-6">
+                  sample paragraph sample paragraph sample paragraph sample
+                  paragraph sample
+                </p>
+              </div>
+            </div>
+            <div>
+              {/* card two */}
+              <div className="bg-gradient-to-b from-pink-800 to-slate-900 text-center rounded-2xl my-5 shadow-sm p-5">
+                <Image
+                  src={code}
+                  alt="designimage"
+                  width={100}
+                  height={100}
+                  className="inline"
+                />
+                <h2 className="text-2xl font-bold text-black">Coding</h2>
+                <p className="text-sm py-2 text-white opacity-20 leading-6">
+                  sample paragraph sample paragraph sample paragraph sample
+                  paragraph sample
+                </p>
+              </div>
+            </div>
+            <div>
+              {/* card three */}
+              <div className="bg-gradient-to-b from-pink-800 to-slate-900 text-center rounded-2xl my-5 shadow-sm p-5">
+                <Image
+                  src={designimage}
+                  alt="designimage"
+                  width={100}
+                  height={100}
+                  className="inline"
+                />
+                <h2 className="text-2xl font-bold text-black">
+                  Graphics Desingning
+                </h2>
+                <p className="text-sm py-2 text-white opacity-20 leading-6">
+                  sample paragraph sample paragraph sample paragraph sample
+                  paragraph sample
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* portpolio  */}
+        <section className="py-4">
+          <div>
+            <h2 className="text-2xl font-bold text-black md:text-3xl mt-4">
+              Portfolio
+            </h2>
+            <p className="text-sm lg:text-lg">
+              sample paragraph sample paragraph sample paragraph sample
+              paragraph sample paragraph sample paragraph sample paragraph
+              sample paragraph sample paragraph sample paragraph sample
+              paragraph sample paragraph
+            </p>
+          </div>
+          {/* images */}
+          <div className="flex gap-10 flex-col p-10 lg:flex-row lg:flex-wrap ">
+            <div className="basis-1/3 flex-1">
+              <Image
+                className=" rounded -lg object-cover"
+                src={web1}
+                alt="web1 image"
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className=" rounded -lgobject-cover "
+                src={web2}
+                alt="web1 image"
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className=" rounded -lgobject-cover "
+                src={web3}
+                alt="web1 image"
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className=" rounded -lgobject-cover "
+                src={web1}
+                alt="web1 image"
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className=" rounded -lgobject-cover "
+                src={web2}
+                alt="web1 image"
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className=" rounded -lgobject-cover "
+                src={web3}
+                alt="web1 image"
+                layout="responsive"
+              />
+            </div>
+          </div>
+        </section>
+        {/* footer */}
+        <footer className=" text-black border-t-2 border-black border-opacity-20 gap-5 flex-col py-5 items-center justify-center lg:flex lg:items-center lg:flex-row">
+          <div>
+            <h3 className="text-base mb-2 lg:text-3xl">
+              Contact me for the more details
+            </h3>
+            <p className=" text-sm opacity-70 lg:text-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+              necessitatibus facilis dicta, ipsa, porro quibusdam a cumque,
+              officia ut commodi accusantium tempore reprehenderit aperiam est
+              vel nisi nam laudantium unde!
+            </p>
+          </div>
+          <div className="flex justify-center gap-10 text-3xl px-20 py-5 text-blue-950 ">
+            <a href="">
+              <BsGithub />
+            </a>
+            <a href="">
+              <BsLinkedin />
+            </a>
+            <a href="">
+              <BsTwitter />
+            </a>
+            <a href="">
+              <BsInstagram />
+            </a>
+          </div>
+        </footer>
+        <div>
+          <h2 className=" text-center font-sans text-gray-500 pb-5 py-5">
+            All Right Reserved 2023
+          </h2>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </main>
+    </div>
+  );
 }
